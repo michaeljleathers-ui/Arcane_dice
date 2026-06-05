65a7ce70dc36fd219393feb545cfebd67d2a3945
michaeljleathers-ui <michaeljleathers@gmail.com>
2026-06-05 13:38:42 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ function rollDice(sides) {
   const result = Math.floor(Math.random() * sides) + 1;
   document.getElementById("result").textContent = result;
 }
+function startAdventure() {
+    document.getElementById("story").textContent =
+        "You stand before an abandoned stone well. The wind is still. Bugs stares into the darkness below.";
+}
